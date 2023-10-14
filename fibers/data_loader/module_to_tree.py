65be0dc8c0da14d516776cd6d24e288891c2e109
Zijian Zhang <doomspec@outlook.com>
2023-10-13 22:46:43 -0400

    feat: add module_to_tree.py loader

---
+++ fibers/data_loader/module_to_tree.py
@@ -0,0 +1,124 @@
+from __future__ import annotations
+
+import inspect
+import json
+
+from moduler.core import build_module_tree
+from moduler import Struct
+from fibers.tree import Node, Tree
+from moduler.docs_parser import \
+    parse_rst_docstring, \
+    parse_google_docstring, Doc_parser
+
+"""
+This modules is for extract the information from python modules and build a tree for it.
+
+
+## Get tree for module
+
+"""
+
+
+def get_tree_for_module(module, docs_parser_type="rst"):
+    if docs_parser_type == "rst":
+        docs_parser = parse_rst_docstring
+    elif docs_parser_type == "google":
+        docs_parser = parse_google_docstring
+    else:
+        raise ValueError("docs_parser_type should be pycharm or vscode")
+    module_name = module.__name__
+    tree = Tree(
+        "Tree of module: " + module_name)
+    module_struct = build_module_tree(module)
+    build_tree_for_struct(module_struct, tree.root, docs_parser)
+    return tree
+
+
+def build_tree_for_struct(curr_struct: Struct, root_note: Node,
+                          docs_parser: Doc_parser):
+    """
+    :param curr_struct: The struct to be added to the child of the root_note
+    :param root_note: The root note to be filled with children from curr_struct
+    :param docs_parser: The parser for the docstring
+    """
+    curr_key = curr_struct.struct_type + ": " + curr_struct.name
+    curr_note = root_note.new_child(curr_key)
+
+    curr_type = "code:" + curr_struct.struct_type
+    curr_obj = curr_struct.obj
+    docs = inspect.getdoc(curr_struct.obj)
+    curr_note.resource.add_resource(curr_obj, curr_type, docs)
+
+    for child_struct in curr_struct.children:
+        match child_struct.struct_type:
+            case "function":
+                process_function_struct(child_struct, curr_note, docs_parser)
+            case "module":
+                build_tree_for_struct(child_struct, curr_note, docs_parser)
+            case "class":
+                build_tree_for_struct(child_struct, curr_note, docs_parser)
+            case "comment":
+                curr_note.be(curr_note.content + "\n" + child_struct.obj)
+            case "section":
+                build_tree_for_struct(child_struct, curr_note, docs_parser)
+            case "todo":
+                build_tree_for_struct(child_struct, curr_note, docs_parser)
+            case "example":
+                build_tree_for_struct(child_struct, curr_note, docs_parser)
+
+
+
+def process_function_struct(function_struct: Struct, parent_node: Node, docs_parser):
+    doc_raw = inspect.getdoc(function_struct.obj)
+    if doc_raw is None:
+        general, parameters, return_value = ("", {}, "")
+    else:
+        general, parameters, return_value = docs_parser(doc_raw)
+    # Check if the function is callable
+    if callable(function_struct.obj):
+        parameters = {**get_empty_param_dict(function_struct.obj), **parameters}
+    else:
+        parameters = {**get_empty_param_dict(function_struct.obj.__func__), **parameters}
+    if "self" in parameters:
+        del parameters["self"]
+    function_name = function_struct.name
+    if not function_name.startswith("__example"):
+        function_note = parent_node.s("function: " + function_name)
+        function_note.be(general)
+        function_note.resource.add_function(function_struct.obj, "function")
+        if len(parameters) > 0:
+            function_note.s("parameters").be(json.dumps(parameters))
+        if len(return_value) > 0:
+            function_note.s("return value").be(return_value)
+    # this is a function demonstrate the usage the module
+    else:
+        function_note = parent_node.s("example: " + function_name)
+        function_code = inspect.getsource(function_struct.obj)
+        # replace first __example with example
+        function_code = function_code.replace("__example", "example", 1)
+        function_note.be(f"{function_code}")
+
+def get_empty_param_dict(func):
+    param_dict = {}
+    for param_name in inspect.signature(func).parameters.keys():
+        param_dict[param_name] = ""
+    return param_dict
+
+def get_docs_in_prompt(doc_tuple):
+    general, params, returns = doc_tuple
+    if len(general) > 0:
+        docs = general + "\n"
+    else:
+        docs = ""
+    for param_name, param_doc in params.items():
+        docs += "Parameter " + param_name + ": " + param_doc + "\n"
+    if len(returns) > 0:
+        docs += "Return value: " + returns + "\n"
+    return docs
+
+
+if __name__ == "__main__":
+    from fibers.testing.testing_modules import v_lab
+
+    tree = get_tree_for_module(v_lab)
+    tree.show_tree_gui()
---
+++ playground/visualize_project.py
@@ -0,0 +1,5 @@
+import fibers
+from fibers.data_loader.module_to_tree import get_tree_for_module
+
+tree = get_tree_for_module(fibers)
+tree.show_tree_gui()
