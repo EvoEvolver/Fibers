e16d7755846076afd535744acf8c7169eb7a6383
Zijian Zhang <doomspec@outlook.com>
2023-10-17 22:23:50 -0400

    docs: add example of debugger usage

---
+++ playground/debugger.py
@@ -0,0 +1,23 @@
+from fibers import debug
+from fibers.helper.cache.cache_service import cached_function, cache_service
+from fibers.model.chat import Chat
+
+
+@cached_function
+def some_function():
+    chat = Chat(system_message="You are a useless helpful assistant.")
+    chat.add_user_message("Randomly give a number in 0 to 10.")
+    res = chat.complete_chat()
+    return res
+
+
+if __name__ == "__main__":
+    with debug.display_chats():
+        print(some_function())
+    cache_service.save_cache()
+    # Because the function is cached, the answer will be the same
+    print(some_function())
+    print(some_function())
+    with debug.refresh_cache():
+        # Because the cache is refreshed, the answer will be different
+        print(some_function())
