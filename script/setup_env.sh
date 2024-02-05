8b7335eaeb27439d42345b95468482c8271c6985
Zijian Zhang <doomspec@outlook.com>
2024-02-05 02:58:29 -0500

    dev: update install script

--- script/setup_env.sh
+++ script/setup_env.sh
@@ -1,6 +1,14 @@
 conda create -n Evo python=3.10
 conda activate Evo # You can replace the name
-conda install nodejs # if you haven't installed it
+# Check whether nodejs is installed
+if which node > /dev/null
+then
+  echo "node is installed"
+else
+  echo "node is not installed"
+  echo "Installing nodejs"
+  conda install nodejs
+fi
 mkdir EvoEvolver # Create the folder for the projects
 cd EvoEvolver
 git clone https://github.com/EvoEvolver/Moduler.git
