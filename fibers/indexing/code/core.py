a75d763787e4998b51dc67a660081658415aed9e
Zijian Zhang <doomspec@outlook.com>
2024-02-11 01:01:57 -0500

    feat: add code indexer

--- fibers/indexing/code/core.py
+++ fibers/indexing/code/core.py
@@ -1,135 +1,91 @@
-from typing import List, Dict
+from typing import List
 
-import numpy as np
-
+from fibers.compose.utils_code.header import get_function_header
 from fibers.data_loader.module_to_tree import get_tree_for_module
-from fibers.indexing.indexing import ComplexIndexing, VectorIndexing
-from fibers.model.embedding import get_embeddings
+from fibers.gui.renderer import Rendered
+from fibers.helper.cache.cache_service import auto_cache, caching
+from fibers.helper.utils import RobustParse, parallel_map
+from fibers.indexing.indexing import VectorIndexing
+from fibers.model.chat import Chat
 from fibers.tree import Tree, Node
+from fibers.tree.node_attr import Attr
+from fibers.tree.node_class import CodeData
 
 
-class CodeIndexing(ComplexIndexing):
-    """
-    query scheme:
-    {
-    "name": "...",
-    "docstring": "..."
-    "parameters": {"param_name": "param_description", ...}
-    }
-    """
-    def __init__(self, tree: Tree):
-        super().__init__(tree.all_nodes())
-        self.indexings = [
-            DescriptionIndexing(tree),
-            ParameterIndexing(tree)
-        ]
+class CodeIndexData(Attr):
+    def __init__(self, node: Node, indexing):
+        super().__init__(node)
+        self.indexing: List[str] = indexing
 
-    def get_similarities(self, query, nodes=None) -> (List[float], List[Node]):
-        desc_indexing = self.indexings[0]
-        param_indexing = self.indexings[1]
-        if len(query) == 1:
-            if query.get("docstring") is not None:
-                return desc_indexing.get_similarities((query["name"], query["docstring"]), nodes)
-            elif query.get("parameters") is not None:
-                return param_indexing.get_similarities(query["parameters"], nodes)
-            else:
-                raise
-        desc_similarities, desc_nodes = desc_indexing.get_similarities((query["name"], query["docstring"]),
-                                                                       nodes)
-        param_similarities, param_nodes = param_indexing.get_similarities(query["parameters"],
-                                                                          nodes)
-        desc_similarities = np.array(desc_similarities)
-        param_similarities = np.array(param_similarities)
-        # Normalize the similarities
-        desc_similarities = desc_similarities / np.max(desc_similarities)
-        param_similarities = param_similarities / np.max(param_similarities)
-        similarity_dict = {}
-        for i, similarity in enumerate(desc_similarities):
-            node = desc_nodes[i]
-            similarity_dict[node] = similarity
-        for i, similarity in enumerate(param_similarities):
-            node = desc_nodes[i]
-            similarity_dict[node] += 0.8 * similarity
-        similarities = list(similarity_dict.values())
-        nodes = list(similarity_dict.keys())
-        return similarities, nodes
+    def render(self, node, rendered: Rendered):
+        res = "<br/>".join(self.indexing)
+        res = f"Code Indexing: {res}"
+        rendered.tabs["Code Indexing"] = res
 
 
-class EnvironmentIndexing(VectorIndexing):
-    def __init__(self, tree: Tree):
-        super().__init__(tree.all_nodes())
+def get_code_indexing(root: Node) -> VectorIndexing:
+    nodes = list(root.iter_subtree_with_dfs())
+    for i, res in parallel_map(gen_code_indexing, nodes):
+        if res is None:
+            continue
+        CodeIndexData(nodes[i], res)
+
+    def get_weighted_contents(node):
+        if not node.has_attr(CodeIndexData):
+            return []
+        indexings = CodeIndexData.get(node).indexing
+        weight = 1 / len(indexings)
+        return [(index, weight) for index in indexings]
+
+    return VectorIndexing(nodes, get_weighted_contents)
 
 
-class DescriptionIndexing(VectorIndexing):
-    """
-    query scheme:
-    (name, description)
-    """
-    def __init__(self, tree: Tree):
-        super().__init__(tree.all_nodes())
-
-    @staticmethod
-    def name_desc_tuple_to_str(name_desc_tuple: (str, str)):
-        name, desc = name_desc_tuple
-        name = name.replace("_", " ")
-        return name + "\n" + desc
-
-    def get_vectors(self, nodes: List[Node]) -> [List[np.ndarray], List[Node]]:
-        non_empty_nodes = []
-        contents = []
-        for node in nodes:
-            if node.resource.has_type("function") and not node.is_empty():
-                non_empty_nodes.append(node)
-                function_name = node.title()
-                contents.append(self.name_desc_tuple_to_str((function_name, node.content)))
-        text_embeddings = get_embeddings(contents)
-        return text_embeddings, non_empty_nodes
-
-    def get_query_vector(self, query: Dict) -> np.ndarray:
-        text_embedding = get_embeddings([self.name_desc_tuple_to_str(query)])
-        return np.array(text_embedding[0])
+def gen_code_indexing(node: Node):
+    if not node.has_attr(CodeData):
+        return
+    code_data = CodeData.get(node)
+    if code_data.module_tree_type != "function":
+        return
+    header = get_function_header(code_data.module_tree_obj)
+    if header.strip() == "":
+        return
+    indexing = break_into_key_sentences(header)
+    return indexing
 
 
-class ParameterIndexing(VectorIndexing):
-    """
-    query scheme:
-    {
-    "param_name": "param_description", ...
-    }
-    """
-    def __init__(self, tree: Tree):
-        super().__init__(tree.all_nodes())
+@auto_cache
+def break_into_key_sentences(func_code: str) -> List[str]:
+    prompt = \
+        f"""
+You are trying to generate some sentences that characterize the code and help others to find it.
+The sentences should together cover the following aspects:
+- the function name
+- what the function does
+- what the function is used for
+- the arguments 
+- the return value of the function.
 
-    @staticmethod
-    def param_dict_to_str(param_dict: Dict[str, str]):
-        param_embed_src = ["Parameters:"]
-        for param_name, param_doc in param_dict.items():
-            if len(param_doc) > 0:
-                param_embed_src.append(param_name + ": " + param_doc)
-            else:
-                param_embed_src.append(param_name)
-        return "\n".join(param_embed_src)
+The code is:
+<code start>
+{func_code}
+<code end>
 
-    def get_vectors(self, nodes: List[Node]) -> [List[np.ndarray], List[Node]]:
-        non_empty_nodes = []
-        contents = []
-        for node in nodes:
-            if node.resource.has_type("function"):
-                parameters = node.resource.get_resource_by_key("parameters")
-                if parameters is None:
-                    continue
-                contents.append(self.param_dict_to_str(parameters))
-                non_empty_nodes.append(node)
-        text_embeddings = get_embeddings(contents)
-        return text_embeddings, non_empty_nodes
-
-    def get_query_vector(self, query: Dict) -> np.ndarray:
-        text_embedding = get_embeddings([self.param_dict_to_str(query)])
-        return np.array(text_embedding[0])
+Output your result as a JSON list of strings. All the strings should start with "The function"
+"""
+    chat = Chat(
+        system_message="You are a smart summarizer of codes who only output in JSON format.")
+    chat.add_user_message(prompt)
+    res = chat.complete_chat()
+    res = RobustParse.list(res)
+    return res
 
 
-if __name__ == "__main__":
-    from fibers.testing.testing_modules import v_lab
-    tree = get_tree_for_module(v_lab)
-    indexing = CodeIndexing(tree)
-    nodes = indexing.get_top_k_nodes({"docstring": "get a beaker of water"}, 2)
+if __name__ == '__main__':
+    import q_lab
+
+    tree = get_tree_for_module(q_lab)
+    indexing = get_code_indexing(tree.root)
+    res = indexing.get_top_k_nodes(["The function plots x y figure"],
+                                   5)
+    print(res)
+    caching.save_used()
