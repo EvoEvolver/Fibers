60d9997a4f3a615c111421201f7028011e727348
Zijian Zhang <doomspec@outlook.com>
2024-05-30 14:42:51 +0800

    docs: add operate module tree

--- playgroud/operate_module_tree.py
+++ playgroud/operate_module_tree.py
@@ -1,10 +1,17 @@
+import inspect
+
 import fibers
 from fibers.data_loader.module_to_tree import get_tree_for_module
-from fibers.tree.node_attr.code_node import get_obj, get_source, get_type
+from fibers.tree.node_attr.code_node import get_obj, get_type
+from fibers.utils.code_process import get_function_header
 
 if __name__ == '__main__':
     root = get_tree_for_module(fibers)
     for node in root.iter_subtree_with_dfs():
         print("---------------")
-        print(get_obj(node))
-        print(get_type(node))
+        obj = get_obj(node)
+        obj_type = get_type(node)
+        print(obj_type)
+        print(obj)
+        if obj_type == "function":
+            print(inspect.getsource(obj))
